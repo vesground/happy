f5bf73a2279c4c7b55bdf273b5b6c0cf9f9395d4
Kyrylo Stas <k.stas@riseapps.biz>
2022-09-25 15:10:20 +0300

    Implement Emotion entity

---
+++ pages/api/emotions.js
@@ -0,0 +1,14 @@
+import { list } from "services/emotions";
+
+export default async function handler(req, res) {
+  const {method, query} = req;
+  let response;
+
+  switch(method) {
+    case 'GET':
+      response = await list(query);
+      break;
+  }
+
+  res.status(200).json(response)
+}
---
+++ services/emotions.js
@@ -0,0 +1,15 @@
+import prisma from "prisma/client"
+
+export async function list({ type, primaryEmotionId }) {
+  const query = { 
+    type: { equals: type }, 
+  };
+
+  if (primaryEmotionId) {
+    query.NOT = { primaryEmotionId: null }
+    query.primaryEmotionId = { equals: Number(primaryEmotionId) }
+  }
+
+  const emotions = await prisma.emotion.findMany({ where: query })
+  return emotions;
+}
