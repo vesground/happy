e84a357ff574a610c4a65ed35b4dd4da38f9e8b9
Kyrylo Stas <k.stas@riseapps.biz>
2022-09-29 14:12:47 +0300

    WIP: Seed emotions

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,13 +1,42 @@
 const { PrismaClient } = require('@prisma/client');
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
 const prisma = new PrismaClient();
 
 const userData = [
   {
-    name: 'Alice',
-    email: 'alice@prisma.io',
+    name: 'vesground',
+    email: 'vesground@gmail.com',
   },
 ];
+
+const csvStream = fs
+  .createReadStream('/Users/vesground/Documents/happy/prisma/emotions.csv')
+  .pipe(parse({ delimiter: ',', from_line: 7 }))
+  .on('data', async function (row) {
+    console.log(row);
+    await insertEmotion(row[0], row[1], row[2]);
+  })
+  .on('end', function () {
+    console.log('finished');
+  })
+  .on('error', function (error) {
+    console.log(error.message);
+  });
+
+async function insertEmotion(name, type, primaryEmotionId) {
+  const data = { name, type };
+
+  if (primaryEmotionId) {
+    data.primaryEmotion = {
+      connect: { id: Number(primaryEmotionId) },
+    };
+  }
+
+  const newEmotion = await prisma.emotion.create({ data });
+  console.log('Successfully inserted emotion', newEmotion.name);
+}
 
 async function main() {
   console.log(`Start seeding ...`);
@@ -17,6 +46,7 @@ async function main() {
     });
     console.log(`Created user with id: ${user.id}`);
   }
+
   console.log(`Seeding finished.`);
 }
 
