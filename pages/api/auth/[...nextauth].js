f31cf310f5e32c30aebea0c01a48a77cb88586cd
Kyrylo Stas <k.stas@riseapps.biz>
2022-10-02 10:44:04 +0300

    Use live data to authenitcate (not mocked)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,8 @@
+import bcrypt from 'bcrypt';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { getByName } from 'services/users';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -14,20 +17,27 @@ export const authOptions = {
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: 'vesground', email: 'vesground@gmail.com' };
+        const user = await getByName(credentials.username);
+        const matched = await bcrypt.compare(credentials.password, user.password);
 
-        if (credentials.username === user.name && credentials.password === 'q123-321Q') {
-          // Any object returned will be saved in `user` property of the JWT
+        if (matched) {
           return user;
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }) {
+      user && (token.user = user);
+      return token;
+    },
+    // Send properties to the client, like an access_token from a provider.
+    async session({ session, token, user }) {
+      session.user = token.user;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
