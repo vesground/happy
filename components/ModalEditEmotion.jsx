27aca16ff3f4ddf032476863742c834509874f4f
Kyrylo Stas <vesground@gmail.com>
2022-10-17 16:33:40 +0300

    Change description in modal

---
+++ components/ModalEditEmotion.jsx
@@ -0,0 +1,34 @@
+import ReactModal from 'react-modal';
+
+import styles from 'styles/ModalEditEmotion.module.scss';
+
+export default function ModalEditEmotion({ isOpen, handleClose, emotionId }) {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const reason = event.target[0].value;
+
+    const data = {
+      userId: 1,
+      emotionId,
+      reason,
+    };
+
+    await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/records`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+    handleClose();
+  }
+
+  return (
+    <ReactModal isOpen={isOpen} contentLabel="Edit emotion">
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <textarea type="text" name="reason" />
+        <div className={styles.buttonGroup}>
+          <button onClick={handleClose}>cancel</button>
+          <button type="submit">save</button>
+        </div>
+      </form>
+    </ReactModal>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from 'styles/Home.module.scss';
 import Layout from 'components/Layout';
 import withAuthentication from 'components/withAuthentication';
 import { fetcher } from 'utils';
+import ModalEditEmotion from 'components/ModalEditEmotion';
 
 function Home() {
   const [selected, setSelected] = useState(null);
@@ -21,23 +22,6 @@ function Home() {
     }
   };
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const reason = event.target[0].value;
-
-    const data = {
-      userId: 1,
-      emotionId: selected.secondary,
-      reason,
-    };
-
-    fetch(`${process.env.NEXT_PUBLIC_HOST}/api/records`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-    });
-    reset();
-  }
-
   function reset() {
     setSelected(null);
     setQuery(`type=primary`);
@@ -49,17 +33,11 @@ function Home() {
         return (
           <div className={styles.emotion} key={emotion.id}>
             <button onClick={handleClick(emotion.type, emotion.id)}>{emotion.name}</button>
-
-            {selected?.secondary === emotion.id && (
-              <form className={styles.form} onSubmit={handleSubmit}>
-                <input type="text" name="reason" />
-                <button type="submit">save</button>
-                <button onClick={reset}>back</button>
-              </form>
-            )}
           </div>
         );
       })}
+
+      <ModalEditEmotion isOpen={!!selected?.secondary} handleClose={reset} emotionId={selected?.secondary} />
     </Layout>
   );
 }
