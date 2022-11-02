c3b71b3553fb6257378624224ecf70506a8edafc
Kyrylo Stas <vesground@gmail.com>
2022-11-02 16:36:42 +0200

    Stream emotions due to too manu to send via a single request

--- services/emotions.js
+++ services/emotions.js
@@ -1,4 +1,5 @@
 import prisma from 'prisma/client';
+import { Readable } from 'stream';
 
 export async function list({ type, primaryEmotionId }) {
   const query = {
@@ -12,4 +13,29 @@ export async function list({ type, primaryEmotionId }) {
 
   const emotions = await prisma.emotion.findMany({ where: query });
   return emotions;
+}
+
+export function stream({ batchSize }) {
+  let cursorId = undefined;
+
+  return new Readable({
+    objectMode: true,
+    async read() {
+      try {
+        const items = await prisma.emotion.findMany({
+          take: batchSize,
+          skip: cursorId ? 1 : 0,
+          cursor: cursorId ? { id: cursorId } : undefined,
+        });
+        if (items.length === 0) {
+          this.push(null);
+        } else {
+          this.push(items);
+          cursorId = items[items.length - 1].id;
+        }
+      } catch (err) {
+        this.destroy(err);
+      }
+    },
+  });
 }
