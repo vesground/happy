14d10a9fdfee6937d5ea199deced8d0775474767
Kyrylo Stas <vesground@gmail.com>
2022-11-06 09:39:19 +0200

    Split emotions selector to a different component

---
+++ components/EmotionsSelect.jsx
@@ -0,0 +1,70 @@
+import { useState, useImperativeHandle, forwardRef } from 'react';
+import useSWR from 'swr';
+import { BiPlus, BiMinus } from 'react-icons/bi';
+
+import { fetcher } from 'utils';
+import Button from 'components/Button';
+
+import styles from 'styles/EmotionsSelect.module.scss';
+
+function EmotionsSelect({ selected, onChange }, ref) {
+  const [opened, setOpened] = useState([]);
+
+  useImperativeHandle(ref, () => ({
+    close: () => {
+      setOpened([]);
+    },
+  }));
+
+  const { data, error } = useSWR(() => `${process.env.NEXT_PUBLIC_HOST}/api/emotions`, fetcher);
+
+  const handleOpen = (type, emotionId) => () => {
+    setOpened((opened) => [...opened, emotionId]);
+  };
+
+  function handleClose() {
+    setOpened((opened) => (opened.length ? opened.slice(0, opened.length - 1) : opened));
+  }
+
+  let filter = filterPrimaryEmotions;
+  if (opened.length === 2) {
+    filter = filterTertiaryEmotions(opened[1]);
+  } else if (opened.length === 1) {
+    filter = filterSecondaryEmotions(opened[0]);
+  }
+
+  const emotions = data?.filter(filter) || [];
+
+  return (
+    <div>
+      {!!opened.length && (
+        <Button className={styles.backBtn} onClick={handleClose}>
+          back
+        </Button>
+      )}
+      {emotions.map((emotion) => (
+        <div className={styles.emotion} key={emotion.id}>
+          <Button
+            type="secondary"
+            className={styles.emotionBtn}
+            key={emotion.id}
+            onClick={handleOpen(emotion.type, emotion.id)}
+          >
+            {emotion.name}
+          </Button>
+          <Button type="icon" onClick={onChange(emotion.id)}>
+            {selected.includes(emotion.id) ? <BiMinus /> : <BiPlus />}
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const filterPrimaryEmotions = (emotion) => emotion.type === 'primary';
+const filterSecondaryEmotions = (primaryEmotionId) => (emotion) =>
+  emotion.type === 'secondary' && primaryEmotionId === emotion.primaryEmotionId;
+const filterTertiaryEmotions = (secondaryEmotionId) => (emotion) =>
+  emotion.type === 'tertiary' && secondaryEmotionId === emotion.primaryEmotionId;
+
+export default forwardRef(EmotionsSelect);
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,23 @@
-import { useState } from 'react';
-import useSWR from 'swr';
-import { BiPlus, BiMinus } from 'react-icons/bi';
+import { useState, useRef } from 'react';
 
 import Layout from 'components/Layout';
 import withAuthentication from 'components/withAuthentication';
-import { fetcher } from 'utils';
 import ModalEditEmotion from 'components/ModalEditEmotion';
 import Button from 'components/Button';
+import EmotionsSelect from 'components/EmotionsSelect';
 
 import styles from 'styles/Home.module.scss';
 
 function Home() {
-  const [opened, setOpened] = useState([]);
   const [selected, setSelected] = useState([]);
+  const selectorRef = useRef();
 
   const [openedModal, setOpenedModal] = useState(false);
-
-  const { data, error } = useSWR(() => `${process.env.NEXT_PUBLIC_HOST}/api/emotions`, fetcher);
-
-  const handleOpen = (type, emotionId) => () => {
-    setOpened((opened) => [...opened, emotionId]);
-  };
 
   const handleSelect = (emotionId) => () => {
     const isSelected = selected.includes(emotionId);
     setSelected((selected) => (isSelected ? selected.filter((id) => id !== emotionId) : [...selected, emotionId]));
   };
-
-  function handleClose() {
-    setOpened((opened) => (opened.length ? opened.slice(0, opened.length - 1) : opened));
-  }
-
-  function reset() {
-    setOpened([]);
-  }
 
   async function createRecord(reason) {
     const data = {
@@ -48,17 +32,8 @@ function Home() {
     });
 
     setOpenedModal(false);
-    reset();
+    selectorRef.currect.close();
   }
-
-  let filter = filterPrimaryEmotions;
-  if (opened.length === 2) {
-    filter = filterTertiaryEmotions(opened[1]);
-  } else if (opened.length === 1) {
-    filter = filterSecondaryEmotions(opened[0]);
-  }
-
-  const emotions = data?.filter(filter) || [];
 
   function openModal() {
     setOpenedModal(true);
@@ -69,45 +44,18 @@ function Home() {
   }
 
   return (
-    <Layout loading={!data} alignY>
-      {emotions.map((emotion) => (
-        <div className={styles.emotion} key={emotion.id}>
-          <Button
-            type="secondary"
-            className={styles.emotionBtn}
-            key={emotion.id}
-            onClick={handleOpen(emotion.type, emotion.id)}
-          >
-            {emotion.name}
-          </Button>
-          <Button type="icon" onClick={handleSelect(emotion.id)}>
-            {selected.includes(emotion.id) ? <BiMinus /> : <BiPlus />}
-          </Button>
-        </div>
-      ))}
+    <Layout alignY>
+      <EmotionsSelect selected={selected} onChange={handleSelect} ref={selectorRef} />
 
-      <div>
-        {!!opened.length && (
-          <Button className={styles.backBtn} onClick={handleClose}>
-            back
-          </Button>
-        )}
-        {!!selected.length && (
-          <Button className={styles.submitBtn} onClick={openModal}>
-            submit
-          </Button>
-        )}
-      </div>
+      {!!selected.length && (
+        <Button className={styles.submitBtn} onClick={openModal}>
+          submit
+        </Button>
+      )}
 
       <ModalEditEmotion isOpen={openedModal} handleClose={closeModal} onSubmit={createRecord} />
     </Layout>
   );
 }
-
-const filterPrimaryEmotions = (emotion) => emotion.type === 'primary';
-const filterSecondaryEmotions = (primaryEmotionId) => (emotion) =>
-  emotion.type === 'secondary' && primaryEmotionId === emotion.primaryEmotionId;
-const filterTertiaryEmotions = (secondaryEmotionId) => (emotion) =>
-  emotion.type === 'tertiary' && secondaryEmotionId === emotion.primaryEmotionId;
 
 export default withAuthentication(Home);
