c60e08e4316275006130363b3ee6de2a1cb2a2dd
Kyrylo Stas <vesground@gmail.com>
2023-04-04 15:17:37 +0300

    Implement custom errors for API

---
+++ helpers/errors.js
@@ -0,0 +1,7 @@
+function CustomError({code, message}) {
+    return JSON.stringify({code, message})
+}
+  
+export function GeneralError(message) {
+    return CustomError({code: 500, message})
+}
--- helpers/response.js
+++ helpers/response.js
@@ -1,9 +1,16 @@
 import { logError } from 'helpers/loggers';
 
 export function handleError(res, error) {
-  logError(error.stack)
-  res.status(500).send({ message: 'Server error. Ask a backend developer to fix it.' })
-}
+    const isUncontrolledError = error instanceof Error;
+    if (isUncontrolledError) {
+      logError(error.stack)
+      res.status(500).send({ message: 'Server error. Ask a backend developer to fix it.' })
+      return;
+    }
+  
+    const { code, message } = JSON.parse(error);
+    res.status(code).send({ message })
+  }
   
 export function handleResponse(res, data) {
   res.status(200).send({ data })
