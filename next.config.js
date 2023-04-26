23a377826975c2f0f3e7c89c8566d42686878fa4
Kyrylo Stas <vesground@gmail.com>
2023-04-26 14:23:38 +0300

    Wrong usage of NextJS config with PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
 });
