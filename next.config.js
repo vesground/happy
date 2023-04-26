26a2a64dc6e9ed39b238fb4e35f70bcefb997512
Kyrylo Stas <vesground@gmail.com>
2023-04-26 14:24:07 +0300

    Linting doesn't work on prettier rules

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'components', 'helpers', 'utils', 'styles', 'services'], // By default, Next.js will run ESLint for all files in the pages/, app (only if the experimental appDir feature is enabled), components/, lib/, and src/ directories.
+    files: ['public/manifest.json']
+  }
 });
